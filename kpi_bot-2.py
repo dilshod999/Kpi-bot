cadbebd6408582f4b9ec4d278a79eafcf9fad8ce
dilshod999 <dilshod26899@gmail.com>
2026-05-12 13:54:59 +0500

    Update kpi_bot-2.py

--- kpi_bot-2.py
+++ kpi_bot-2.py
@@ -288,6 +288,7 @@ async def show_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT, handle_text))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
+app.add_handler(MessageHandler(filters.FORWARDED, handle_text))
     print("🤖 Бот запущен...")
     app.run_polling()
